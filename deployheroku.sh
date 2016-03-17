0abbcc2a8c03e971f02a4d222310f3be16b5048c
Matthew Whisenhunt <mattwhisenhunt@gmail.com>
2016-03-17 15:05:30 -0700

    Force deplot to Heroku.

--- deployheroku.sh
+++ deployheroku.sh
@@ -4,7 +4,7 @@ git checkout -b heroku
 godep save ./...
 git add -f vendor Godeps
 git commit -m "Heroku Deploy"
-git push heroku master
+git push -f HEAD heroku
 git checkout master
 git branch -D heroku
 rm -r vendor Godeps
